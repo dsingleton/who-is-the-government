88162ce8956f47937c8ffc5b886b7d1e24fd538e
David Singleton <david@dsingleton.co.uk>
2015-03-27 20:50:24 +0000

    Show the name of the latest government
    Fetch from the GOV.UK API, which returns in reverse chronological order,
    making sure to check the latest goverment doesn't have an end date in
    the past. As it's possible that the current government could end before
    a new government is created.

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
+require 'net/http'
+require 'json'
+require 'date'
 
 get '/' do
-  "Hello World!"
+  gov = current_government
+  return gov['title'] if gov
+  "There is no government, right now."
+end
+
+def current_government
+  response = Net::HTTP.get_response(URI.parse("https://www.gov.uk/api/governments"))
+  latest = JSON.parse(response.body)['results'].first
+  end_date = latest['details']['end_date']
+  latest unless end_date && Date.parse(end_date) <= Date.today
 end
